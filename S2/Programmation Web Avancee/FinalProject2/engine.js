c7ab0ceb50a1e1a5543174596956eb6d2abb4f83
FrelVick <FrelVick2@ya.ru>
2017-04-14 23:19:50 +0200

    game ~finish

--- S2/Programmation Web Avancee/FinalProject2/engine.js
+++ S2/Programmation Web Avancee/FinalProject2/engine.js
@@ -29,110 +29,111 @@ class Engine {
     update(dt) {
 
         this.dynamic_bodies.forEach(function (item, curr, dynamic_bodies) {
-                let top_left_interaction = item.origin.add(item.interaction_left_top);
-                let right_down_interaction = item.origin.add(item.interaction_right_down);
-                dynamic_bodies[curr].state.status = "wait";
-                // 1. recalculating of acceleration
-                if (this.key_pressed.left !== this.key_pressed.right) {
-                    dynamic_bodies[curr].state.to_left = false;
-                    if (this.key_pressed.boost) {
-                        dynamic_bodies[curr].maxspeed = Constants.max_boosted_speed;
-                        dynamic_bodies[curr].acceleration = Constants.boosted_acceleration;
-                    } else {
-                        dynamic_bodies[curr].maxspeed = Constants.max_speed;
-                        dynamic_bodies[curr].acceleration = Constants.acceleration;
-                    }
-                    if (this.key_pressed.left) {
-                        dynamic_bodies[curr].state.to_left = true;
-                        dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.invert_x()
-                    }
+            let top_left_interaction = item.origin.add(item.interaction_left_top);
+            let right_down_interaction = item.origin.add(item.interaction_right_down);
+            dynamic_bodies[curr].state.status = "wait";
+            // 1. recalculating of acceleration
+            if (this.key_pressed.left !== this.key_pressed.right) {
+                dynamic_bodies[curr].state.to_left = false;
+                if (this.key_pressed.boost) {
+                    dynamic_bodies[curr].maxspeed = Constants.max_boosted_speed;
+                    dynamic_bodies[curr].acceleration = Constants.boosted_acceleration;
                 } else {
-                    item.maxspeed = Constants.max_vertical_speed_v;
-                    dynamic_bodies[curr].acceleration = Constants.gravity;
+                    dynamic_bodies[curr].maxspeed = Constants.max_speed;
+                    dynamic_bodies[curr].acceleration = Constants.acceleration;
                 }
-
-                // 2. collision
-                dynamic_bodies[curr].state.on_floor = false;
-                for (let j = 0; j < this.static_bodies.length; j++) {
-                    /*if (this.static_bodies[j].origin.x > top_left_interaction.x &&
-                        this.static_bodies[j].origin.y > top_left_interaction.y &&
-                        this.static_bodies[j].origin.x < right_down_interaction.x &&
-                        this.static_bodies[j].origin.y < right_down_interaction.y) {*/
-                        let res = item.collision(this.static_bodies[j]);
-
-                        if (res !== null) {
-                            if (this.static_bodies[j])
-                            if (res.y < 0) {
-                                dynamic_bodies[curr].state.on_floor = true;
-                            }
-                            res = res.axe_not_moved();
-                            dynamic_bodies[curr].velocity = dynamic_bodies[curr].velocity.mult_v(res);
-                            dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.mult_v(res);
-                        }
-                    //}
+                if (this.key_pressed.left) {
+                    dynamic_bodies[curr].state.to_left = true;
+                    dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.invert_x()
                 }
-
-                // 2.1 adding y
-                //TODO пропуск кадров перед отметкой на земле
-                if (this.key_pressed.up) {
-                    this.key_pressed.up = false;
-                    if (dynamic_bodies[curr].state.on_floor) {
-                        dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.add(Constants.jump_acceleration);
-                        dynamic_bodies[curr].state.double_jump = false;
-                    }
-                    /*else {
-                     if (!(dynamic_bodies[curr].state.double_jump)) {
-                     dynamic_bodies[curr].state.double_jump = true;
-                     //добавить маленькое ускорение
-                     dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.add(
-                     Constants.jump_acceleration.mult_v(new Vector(Math.pow(0.1, Math.abs(dynamic_bodies[curr].velocity.y)), 1)));
-                     console.log(Math.pow(0.01, Math.abs(dynamic_bodies[curr].velocity.y)), Constants.jump_acceleration.x * Math.pow(0.01, Math.abs(dynamic_bodies[curr].velocity.y)));
-                     }
-                     }*/
-                }
-
-                // 3. recalculating of speed
-
-                dynamic_bodies[curr].velocity = dynamic_bodies[curr].velocity.add_wo_overflow(
-                    dynamic_bodies[curr].acceleration.mult(dt), dynamic_bodies[curr].maxspeed);
-                dynamic_bodies[curr].acceleration = Vector.ZERO;
-                // 4. deceleration
-
-                dynamic_bodies[curr].velocity = Math.abs(dynamic_bodies[curr].velocity.x) > dynamic_bodies[curr].maxspeed.x ?
-                    dynamic_bodies[curr].velocity.mult_v(Constants.deceleration.pow(dt)) :
-                    dynamic_bodies[curr].velocity;
-
-                dynamic_bodies[curr].velocity = Math.abs(item.velocity.y) < Constants.min_speed.y ?
-                    dynamic_bodies[curr].velocity.mult_v(Vector.UNIT_X) : dynamic_bodies[curr].velocity;
-
-                // 5. recalculating of position
-
-                dynamic_bodies[curr].move(dynamic_bodies[curr].velocity.mult(dt));
-                // 6. update character status
-                if (item.velocity.y > 0) {
-                    dynamic_bodies[curr].state.status = "jump_up";
-                } else {
-                    if (item.velocity.y < 0) {
-                        dynamic_bodies[curr].state.status = "jump_down";
-                    } else {
-                        if (item.velocity.x !== 0) {
-                            if (item.maxspeed === Constants.max_boosted_speed) {
-                                dynamic_bodies[curr].state.status = "run";
-                            } else {
-                                dynamic_bodies[curr].state.status = "walk";
-                            }
-                        }
-                    }
-                }
-
-
+            } else {
+                item.maxspeed = Constants.max_vertical_speed_v;
+                dynamic_bodies[curr].acceleration = Constants.gravity;
             }
 
-            ,
-            this
-        )
-        ;
+            // 2. collision
+            dynamic_bodies[curr].state.on_floor = false;
+            for (let j = 0; j < this.static_bodies.length; j++) {
+                /*if (this.static_bodies[j].origin.x > top_left_interaction.x &&
+                 this.static_bodies[j].origin.y > top_left_interaction.y &&
+                 this.static_bodies[j].origin.x < right_down_interaction.x &&
+                 this.static_bodies[j].origin.y < right_down_interaction.y) {*/
+                let res = item.collision(this.static_bodies[j]);
 
+                if (res !== null) {
+                    if (this.static_bodies[j].obstacle) {
+                        if (res.y < 0) {
+                            dynamic_bodies[curr].state.on_floor = true;
+                        }
+                        res = res.axe_not_moved();
+                        dynamic_bodies[curr].velocity = dynamic_bodies[curr].velocity.mult_v(res);
+                        dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.mult_v(res);
+                    } else {
+                        if (this.static_bodies[j].death) {
+                            console.log("death")
+                        } else {
+                            if (this.static_bodies[j].target) {
+                                this.static_bodies.splice(j, 1);
+                                dynamic_bodies[curr].state.type += 1;
+                            }
+                        }
+                    }
+                }
+            }
 
+            // 2.1 adding y
+            //TODO пропуск кадров перед отметкой на земле
+            if (this.key_pressed.up) {
+                this.key_pressed.up = false;
+                if (dynamic_bodies[curr].state.on_floor) {
+                    dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.add(Constants.jump_acceleration);
+                    dynamic_bodies[curr].state.double_jump = false;
+                }
+                /*else {
+                 if (!(dynamic_bodies[curr].state.double_jump)) {
+                 dynamic_bodies[curr].state.double_jump = true;
+                 //добавить маленькое ускорение
+                 dynamic_bodies[curr].acceleration = dynamic_bodies[curr].acceleration.add(
+                 Constants.jump_acceleration.mult_v(new Vector(Math.pow(0.1, Math.abs(dynamic_bodies[curr].velocity.y)), 1)));
+                 console.log(Math.pow(0.01, Math.abs(dynamic_bodies[curr].velocity.y)), Constants.jump_acceleration.x * Math.pow(0.01, Math.abs(dynamic_bodies[curr].velocity.y)));
+                 }
+                 }*/
+            }
+
+            // 3. recalculating of speed
+
+            dynamic_bodies[curr].velocity = dynamic_bodies[curr].velocity.add_wo_overflow(
+                dynamic_bodies[curr].acceleration.mult(dt), dynamic_bodies[curr].maxspeed);
+            dynamic_bodies[curr].acceleration = Vector.ZERO;
+            // 4. deceleration
+
+            dynamic_bodies[curr].velocity = Math.abs(dynamic_bodies[curr].velocity.x) > dynamic_bodies[curr].maxspeed.x ?
+                dynamic_bodies[curr].velocity.mult_v(Constants.deceleration.pow(dt)) :
+                dynamic_bodies[curr].velocity;
+
+            dynamic_bodies[curr].velocity = Math.abs(item.velocity.y) < Constants.min_speed.y ?
+                dynamic_bodies[curr].velocity.mult_v(Vector.UNIT_X) : dynamic_bodies[curr].velocity;
+
+            // 5. recalculating of position
+
+            dynamic_bodies[curr].move(dynamic_bodies[curr].velocity.mult(dt));
+            // 6. update character status
+            if (item.velocity.y > 0) {
+                dynamic_bodies[curr].state.status = "jump_up";
+            } else {
+                if (item.velocity.y < 0) {
+                    dynamic_bodies[curr].state.status = "jump_down";
+                } else {
+                    if (item.velocity.x !== 0) {
+                        if (item.maxspeed === Constants.max_boosted_speed) {
+                            dynamic_bodies[curr].state.status = "run";
+                        } else {
+                            dynamic_bodies[curr].state.status = "walk";
+                        }
+                    }
+                }
+            }
+
+        }, this);
     }
 }
