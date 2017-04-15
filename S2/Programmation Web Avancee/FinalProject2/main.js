30519dbfd36096951bd065cacb57c985869da4da
FrelVick <FrelVick2@ya.ru>
2017-04-15 10:18:06 +0200

    game ~finish

--- S2/Programmation Web Avancee/FinalProject2/main.js
+++ S2/Programmation Web Avancee/FinalProject2/main.js
@@ -10,10 +10,10 @@ let renderer;
 
 let init = new Init();
 
-let to_preload=[];
+let to_preload = [];
 let x;
 
-let phase0 = function (){
+let phase0 = function () {
     let checkImage = path =>
         new Promise(resolve => {
             let img = new Image();
@@ -34,11 +34,22 @@ let phase0 = function (){
     to_preload.push("img/skill-desc_0001_buildings.png");
     to_preload.push("img/skill-desc_0001_buildings.png");
     to_preload.push("img/characters.png");
-    x = loadImg(to_preload)/*.all(phase1(level1_description));*/
-    x.then(res=>phase1(level1_description));
+    x = loadImg(to_preload);
+    x.then(res => phase1(level1_description));
 };
 
 let phase1 = function (level1_description) {
+    let canvases = document.getElementsByClassName("canvas");
+    if (window.innerWidth / 272 >
+        window.innerHeight / 160) {
+        for (let i = 0; i < canvases.length; i++) {
+            canvases[i].style.height = "100%"
+        }
+    } else {
+        for (let i = 0; i < canvases.length; i++) {
+            canvases[i].style.width = "100%"
+        }
+    }
     init.imgs_back = [new Image(), new Image(), new Image(), new Image()];
     init.imgs_back[3].src = "img/skill-desc_0003_bg.png";
     init.imgs_back[2].src = "img/skill-desc_0002_far-buildings.png";
@@ -46,8 +57,6 @@ let phase1 = function (level1_description) {
     init.imgs_back[0].src = "img/skill-desc_0000_foreground.png";
     init.cnvs_back = ["back4", "back3", "back2", "back1"];
     init.tile_img = new Image();
-
-    //let level1_description = JSON.parse('{"height":10,"layers":[{"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,409,404,404,404,404,404,404,404,404,404,404,404,404,406,409,404,404,404,404,404,404,404,404,404,404,404,406,342,325,343,0,0,405,0,0,0,0,0,6,8,0,0,0,0,0,408,407,0,0,0,0,426,428,0,0,0,0,0,408,404,306,331,0,404,407,0,0,312,312,312,312,312,312,312,312,0,0,0,0,0,732,732,0,0,0,0,732,732,0,0,0,341,841,344,0,0,0,0,0,335,28,26,26,26,26,27,221,0,0,0,0,0,335,448,0,352,353,0,447,641,0,0,0,0,0,0,755,87,219,219,219,333,118,772,348,348,773,119,334,219,219,219,219,219,333,538,352,0,0,353,539,754,639,639,639,639,639,639,753,0,0,0,0,0,120,775,350,350,774,121,0,0,0,0,0,0,0,120,775,350,350,774,121,0,0,0,0,0,0,0,0],"height":10,"name":"level","opacity":1,"type":"tilelayer","visible":true,"width":32,"x":0,"y":0}],"nextobjectid":1,"orientation":"orthogonal","renderorder":"left-up","tileheight":16,"tilesets":[{"columns":20,"firstgid":1,"image":"img/bofhblkalpha.png","imageheight":800,"imagewidth":320,"margin":0,"name":"bofhblkalpha","spacing":0,"tilecount":1000,"tileheight":16,"tileproperties":{"5":{"obstacle":true},"7":{"obstacle":true},"24":{"obstacle":true},"25":{"obstacle":true},"26":{"obstacle":true},"27":{"obstacle":true},"86":{"obstacle":true},"218":{"obstacle":true},"220":{"obstacle":true},"334":{"obstacle":true},"351":{"death":true},"352":{"death":true},"425":{"obstacle":true},"427":{"obstacle":true},"446":{"obstacle":true},"447":{"obstacle":true},"638":{"obstacle":true},"640":{"obstacle":true},"754":{"obstacle":true},"840":{"target":true}},"tilepropertytypes":{"5":{"obstacle":"bool"},"7":{"obstacle":"bool"},"24":{"obstacle":"bool"},"25":{"obstacle":"bool"},"26":{"obstacle":"bool"},"27":{"obstacle":"bool"},"86":{"obstacle":"bool"},"218":{"obstacle":"bool"},"220":{"obstacle":"bool"},"334":{"obstacle":"bool"},"351":{"death":"bool"},"352":{"death":"bool"},"425":{"obstacle":"bool"},"427":{"obstacle":"bool"},"446":{"obstacle":"bool"},"447":{"obstacle":"bool"},"638":{"obstacle":"bool"},"640":{"obstacle":"bool"},"754":{"obstacle":"bool"},"840":{"target":"bool"}},"tilewidth":16}],"tilewidth":16,"version":1,"width":32}');
 
     init.tile_img.src = level1_description.tilesets[0].image;
 
@@ -154,6 +163,6 @@ let phase3 = function (string) {
 };
 
 //document.onload = phase0();
-window.addEventListener("DOMContentLoaded", phase0(),false);
+window.addEventListener("DOMContentLoaded", phase0(), false);
 
 //end test
