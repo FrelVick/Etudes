cf2b1a2319b839f101b495efbae0d045cad73672
Linus Torvalds <torvalds@linux-foundation.org>
2017-09-21 22:52:05 +0200

    1

---
+++ M2/S1/Langages centres donnes/TP3/LCD_TP3_ACOMPLETER/src/lcd/tp03/XPathUnion.java
@@ -0,0 +1,67 @@
+package lcd.tp03;
+
+import java.util.Vector;
+
+import org.w3c.dom.Node;
+
+class XPathUnion extends XPathExpr {
+
+	XPathUnion() {
+		super(2);
+	}
+
+	@Override
+	Vector<Node> eval() {
+		Vector<Node> set1 = arguments[0].eval();
+		Vector<Node> set2 = arguments[1].eval();
+		Vector<Node> res = new Vector<>();
+
+		/****
+		 * A COMPLETER : res doit contenir l'union des deux vecteurs set1 et set2 (qui sont triés dans l'ordre du document)
+		 * Le numéro prefixe d'un nœud peut s'obtenir grace à
+		 * Integer pre = (Integer) n.getUserData("preorder");
+		 */
+		int i = 0;
+		int j = 0;
+		while (i < set1.size() && j < set2.size()) {
+			Node n1 = set1.get(i);
+			Node n2 = set2.get(j);
+
+			Integer pre1 = (Integer) n1.getUserData("preorder");
+			Integer pre2 = (Integer) n2.getUserData("preorder");
+
+			if (pre1.equals(pre2)) {
+				res.add(n1);
+				i++;
+				j++;
+			} else if (pre1 < pre2) {
+				res.add(n1);
+				i++;
+			} else {
+				res.add(n2);
+				j++;
+			}
+
+		};
+		while (i < set1.size()) {
+			Node n1 = set1.get(i);
+			res.add(n1);
+			i++;
+		};
+		while (j < set2.size()) {
+			Node n2 = set2.get(j);
+			res.add(n2);
+			j++;
+		};
+
+
+		return res;
+
+	}
+
+	@Override
+	String getLabel() {
+		return "Union";
+	}
+
+}
--- S2/Programmation Web Avancee/FinalProject/tile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by wtflocal on 12.03.2017.
- */
-"use strict";
-class Tile {
-    append(propertyname, value) {
-        this[propertyname] = value;
-    }
-}
